c83cb4ccae54c597dfe980867c8be04a50645258
fathironmy2 <fathironmy2@gmail.com>
2025-04-25 08:15:44 +0000

    blog

--- api/server.js
+++ api/server.js
@@ -3,13 +3,13 @@ const jsonServer = require('json-server')
 
 const server = jsonServer.create()
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+Uncomment to allow write operations
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
 // Comment out to allow write operations
 const router = jsonServer.router('db.json')
@@ -24,7 +24,7 @@ server.use(jsonServer.rewriter({
 }))
 server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running')
+    console.log('JSON Server is running http://localhost:3000')
 })
 
 // Export the Server API
